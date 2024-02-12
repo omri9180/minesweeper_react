9db717aae5691c06a9625cad68774d46540d91e2
Omri Vaturi <omri1991@gmail.com>
2024-02-12 15:44:09 +0200

    timer work

--- src/Components/Clock.jsx
+++ src/Components/Clock.jsx
@@ -1,3 +1,30 @@
-const Clock = () => {};
+import React, { useEffect, useState } from 'react';
+
+const Clock = ({ setTimeCount, timeCount, startClock }) => {
+    useEffect(() => {
+        let intervalId;
+        if (startClock) {
+            intervalId = setInterval(() => {
+                setTimeCount(prevTime => {
+                    let [hundreds, tens, ones] = prevTime;
+                    ones++;
+                    if (ones === 10) {
+                        ones = 0;
+                        tens++;
+                    }
+                    if (tens === 10) {
+                        tens = 0;
+                        hundreds++;
+                    }
+                    return [hundreds, tens, ones];
+                });
+            }, 1000);
+        }
+
+        return () => clearInterval(intervalId);
+    }, [startClock, setTimeCount]);
+
+    return <div>{timeCount[0] + '' + timeCount[1] + '' + timeCount[2]}</div>;
+};
 
 export default Clock;
