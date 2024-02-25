643dbdee242937014df4da02f3bfef28236c5bff
Vaturi <omri1991@gmail.com>
2024-02-25 09:58:38 +0200

    count number of mines around each cell

--- src/Components/Cell.jsx
+++ src/Components/Cell.jsx
@@ -3,6 +3,7 @@ import './Styles/Cell.css';
 
 const Cell = ({ numRows, numCols, numMines, startGame }) => {
     const [board, setBoard] = useState([]);
+    const [countMines, setCountMines] = useState(0);
     useEffect(() => {
         const boardCells = Array.from({ length: numRows }, () =>
             Array.from({ length: numCols }, () => ({
@@ -22,16 +23,49 @@ const Cell = ({ numRows, numCols, numMines, startGame }) => {
                 minesPlaced++;
             }
         }
+        boardCells.forEach((row, rowIndex) => {
+            row.forEach((cell, colIndex) => {
+                if (cell.isMine) return;
+                cell.neighborCount = getNeighborMines(boardCells, rowIndex, colIndex);
+            });
+        });
         setBoard(boardCells);
     }, []);
+
+    const getNeighborMines = (boardCells, row, col) => {
+        let count = 0;
+        for (let i = -1; i <= 1; i++) {
+            for (let j = -1; j <= 1; j++) {
+                if (i === 0 && j === 0) continue;
+                const newRow = row + i;
+                const newCol = col + j;
+                if (
+                    newRow >= 0 &&
+                    newRow < numRows &&
+                    newCol >= 0 &&
+                    newCol < numCols &&
+                    boardCells[newRow][newCol].isMine
+                ) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    };
+
+    const handleCellClick = (row, col) => {
+        if (board[row][col].neighborCount > 0) {
+            return;
+        }
+    };
 
     return (
         <>
             {board.map((row, rowIndex) => (
                 <div key={rowIndex} className="row">
                     {row.map((cell, colIndex) => (
-                        <div key={colIndex} className="cell" onClick={startGame}>
-                            {cell.isMine ? '☢️' : ''}
+                        <div key={colIndex} className="cell" onClick={handleCellClick(rowIndex, colIndex)}>
+                            {cell.isMine ? '☢️' : board[rowIndex][colIndex].neighborCount}
                         </div>
                     ))}
                 </div>
